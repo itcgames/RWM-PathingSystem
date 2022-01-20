2d1713c447bb2e47e928e98b73314333a1b49939
ben-millar <c00236772@itcarlow.ie>
2022-01-20 18:36:58 +0000

    Added alignment types

--- Packages/ie.itcarlow.pathingsystem/Runtime/BezierSpline.cs
+++ Packages/ie.itcarlow.pathingsystem/Runtime/BezierSpline.cs
@@ -9,6 +9,9 @@ public class BezierSpline : MonoBehaviour
     private Vector3[] points;
 
     [SerializeField]
+    private BezierControlPointMode[] modes;
+
+    [SerializeField]
     private bool loop;
 
     public bool Loop
@@ -19,6 +22,7 @@ public class BezierSpline : MonoBehaviour
             loop = value;
             if (value == true)
             {
+                modes[modes.Length - 1] = modes[0];
                 SetControlPoint(0, points[0]);
             }
         }
@@ -72,8 +76,74 @@ public class BezierSpline : MonoBehaviour
         }
 
         points[index] = point;
+        EnforceMode(index);
     }
 
+    public BezierControlPointMode GetControlPointMode(int index)
+    {
+        return modes[(index + 1) / 3];
+    }
+
+    public void SetControlPointMode(int index, BezierControlPointMode mode)
+    {
+        int modeIndex = (index + 1) / 3;
+        modes[modeIndex] = mode;
+        if (loop)
+        {
+            if (modeIndex == 0)
+                modes[modes.Length - 1] = mode;
+            else if (modeIndex == modes.Length - 1)
+                modes[0] = mode;
+        }
+
+        EnforceMode(index);
+    }
+
+    private void EnforceMode(int index)
+    {
+        int modeIndex = (index + 1) / 3;
+        BezierControlPointMode mode = modes[modeIndex];
+        if (mode == BezierControlPointMode.Free || !loop && (modeIndex == 0 || modeIndex == modes.Length - 1))
+        {
+            return;
+        }
+
+        int middleIndex = modeIndex * 3;
+        int fixedIndex, enforcedIndex;
+
+        if (index <= middleIndex)
+        {
+            fixedIndex = middleIndex - 1;
+
+            if (fixedIndex < 0)
+                fixedIndex = points.Length - 2;
+
+            enforcedIndex = middleIndex + 1;
+
+            if (enforcedIndex >= points.Length)
+                enforcedIndex = 1;
+        }
+        else
+        {
+            fixedIndex = middleIndex + 1;
+
+            if (fixedIndex >= points.Length)
+                fixedIndex = 1;
+
+            enforcedIndex = middleIndex - 1;
+
+            if (enforcedIndex < 0)
+                enforcedIndex = points.Length - 2;
+        }
+
+        Vector3 middle = points[middleIndex];
+        Vector3 enforcedTangent = middle - points[fixedIndex];
+
+        if (mode == BezierControlPointMode.Aligned)
+            enforcedTangent = enforcedTangent.normalized * Vector3.Distance(middle, points[enforcedIndex]);
+
+        points[enforcedIndex] = middle + enforcedTangent;
+    }
 
     public void Reset()
     {
@@ -83,6 +153,11 @@ public class BezierSpline : MonoBehaviour
             new Vector3(2f, 0f, 0f),
             new Vector3(3f, 0f, 0f),
             new Vector3(4f, 0f, 0f)
+        };
+
+        modes = new BezierControlPointMode[] {
+            BezierControlPointMode.Free,
+            BezierControlPointMode.Free
         };
     }
 
@@ -97,10 +172,15 @@ public class BezierSpline : MonoBehaviour
             points[points.Length - i] = point;
         }
 
+        Array.Resize(ref modes, modes.Length + 1);
+        modes[modes.Length - 1] = modes[modes.Length - 2];
+        EnforceMode(points.Length - 4);
 
         if (loop)
         {
             points[points.Length - 1] = points[0];
+            modes[modes.Length - 1] = modes[0];
+            EnforceMode(0);
         }
     }
 
