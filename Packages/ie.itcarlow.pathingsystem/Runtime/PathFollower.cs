70e251b4b45c4c472e6070713b6de17efec57df6
ben-millar <c00236772@itcarlow.ie>
2022-01-21 16:31:28 +0000

    Added PathFollower script

---
+++ Packages/ie.itcarlow.pathingsystem/Runtime/PathFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    public BezierSpline spline;
+    public float duration;
+
+    private float progress;
+
+    void Update()
+    {
+        progress += Time.deltaTime / duration;
+        if (progress > 1f)
+            progress -= 1f;
+
+        transform.localPosition = spline.GetPoint(progress);
+    }
+}
